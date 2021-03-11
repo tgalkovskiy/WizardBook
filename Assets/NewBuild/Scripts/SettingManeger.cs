4e66d5b84fa5b45ea71e482c0d4fb415b5727a8e
tgalkovskiy <56587478+tgalkovskiy@users.noreply.github.com>
2021-03-11 14:31:29 +0300

    new

---
+++ Assets/NewBuild/Scripts/SettingManeger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingManeger : MonoBehaviour
+{
+    [SerializeField] private Setting Setting  = default;
+    [SerializeField] private AudioSource Volume = default;
+    [SerializeField] private Slider Volum_Slider = default;
+
+    private void Awake()
+    {
+        Setting.LoadSetting();
+        Volume.volume = Setting._Volume;
+        Volum_Slider.value = Setting._Volume;
+    }
+    public void ChangeVolume()
+    {
+        Volume.volume = Volum_Slider.value;
+        Setting._Volume = Volume.volume;
+        Setting.SaveSetting();
+    }
+    
+}
---
+++ Assets/NewBuild/scriptbleObj/Setting.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "SettingPreset", menuName = "SettingGame")]
+public class Setting : ScriptableObject
+{
+    public class SettingData
+    {
+        public float Volume;
+    }
+    public float _Volume;
+    private void Awake()
+    {
+        LoadSetting();
+    }
+    public void SaveSetting()
+    {
+        string Path = System.IO.Path.Combine(Application.persistentDataPath, "SaveSetting.Json");
+        SettingData settingData = new SettingData();
+        settingData.Volume = _Volume;
+        try
+        {
+            File.WriteAllText(Path, JsonUtility.ToJson(settingData));
+        }
+        catch
+        {
+            Debug.Log("not SaveSetting");
+        }
+        finally
+        {
+            Debug.Log("Save Done Setting");
+        }
+    }
+    public void LoadSetting()
+    {
+        string Path = System.IO.Path.Combine(Application.persistentDataPath, "SaveSetting.Json");
+        if (File.Exists(Path))
+        {
+            SettingData settingData = new SettingData();
+            settingData = JsonUtility.FromJson<SettingData>(File.ReadAllText(Path));
+            _Volume = settingData.Volume;
+        }
+        else
+        {
+            Debug.Log("No SaveSetting");
+            _Volume = 1;
+        }
+    }
+}
