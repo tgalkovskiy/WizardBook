2b9645fad98755a07098b74485c058db4d7586ca
tgalkovskiy <80895083+GalkovskiyTimur@users.noreply.github.com>
2021-04-27 13:59:33 +0300

    fix

--- Assets/NewBuild/Scripts/Map/Bion_Shois.cs
+++ Assets/NewBuild/Scripts/Map/Bion_Shois.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bion_Shois : MonoBehaviour
 {
@@ -9,9 +10,18 @@ public class Bion_Shois : MonoBehaviour
     [SerializeField] private GameObject PanelOf = default;
     [SerializeField] private ChoisEnemy ChoisEnemy;
     [SerializeField] private Map map_Setting;
+    [SerializeField] private HP _hp =default;
+
+    [SerializeField] private Image _map2 = default;
+    [SerializeField] private Image _map3 = default;
+
+    [SerializeField] private Sprite[] _iconMap = default;
+    [SerializeField] private GameObject[] _text = default;
     
     [SerializeField] private SaveTutorial _saveTutorial = default;
     [SerializeField] private GameObject Map_tutorial = default;
+    
+    
 
     private void Start()
     {
@@ -23,35 +33,56 @@ public class Bion_Shois : MonoBehaviour
             _saveTutorial.first_map = true;
             _saveTutorial.SaveData();
         }
+
+        if (_hp.LVLBooK >= 3)
+        {
+            _map2.sprite = _iconMap[0];
+            _text[0].SetActive(false);
+        }
+
+        if (_hp.LVLBooK >= 6)
+        {
+            _map3.sprite = _iconMap[1];
+            _text[1].SetActive(false);
+        }
     }
 
     public void Chois(int NumberTerrain)
     {
         if (NumberTerrain < Terrain.Length)
         {
-            Instantiate(Terrain[NumberTerrain]);
-            map_Setting.LoadData();
             if (NumberTerrain==0)
             {
                 ChoisEnemy.maxEntmy = map_Setting.Number_Max1;
                 map_Setting.Now_map = 0;
+                ChoisEnemy.enabled = true;
+                this.gameObject.SetActive(false);
+                Instantiate(Terrain[NumberTerrain]);
+                map_Setting.LoadData();
             }
-            if (NumberTerrain==1)
+            if (NumberTerrain==1 && _hp.LVLBooK>=3)
             {
                 ChoisEnemy.maxEntmy = map_Setting.Number_Max2;
                 map_Setting.Now_map = 1;
+                ChoisEnemy.enabled = true;
+                this.gameObject.SetActive(false);
+                Instantiate(Terrain[NumberTerrain]);
+                map_Setting.LoadData();
             }
-            if (NumberTerrain==2)
+            if (NumberTerrain==2 && _hp.LVLBooK>=6)
             {
                 ChoisEnemy.maxEntmy = map_Setting.Number_Max3;
                 map_Setting.Now_map = 2;
+                ChoisEnemy.enabled = true;
+                this.gameObject.SetActive(false);
+                Instantiate(Terrain[NumberTerrain]);
+                map_Setting.LoadData();
             }
-            this.gameObject.SetActive(false);
         }
         else
         {
             PanelOf.SetActive(true);
         }
-        ChoisEnemy.enabled = true;
+        
     }
 }
--- Assets/NewBuild/Scripts/Other/PageManeger.cs
+++ Assets/NewBuild/Scripts/Other/PageManeger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
@@ -26,7 +27,34 @@ public class PageManeger : MonoBehaviour
     [SerializeField] private Text Gold = default;
     [SerializeField] private Text Rubin = default;
     [SerializeField] private Text Energy = default;
-   
+
+    [SerializeField] private Sprite[] _imagesMenu = default;
+    [SerializeField] private Image _imagesBook = default;
+    [SerializeField] private Image _imagesUp = default;
+    [SerializeField] private Image _imagesDown = default;
+
+    private void OnEnable()
+    {
+        if (StatPers.LVLBooK <= 3)
+        {
+            _imagesBook.sprite = _imagesMenu[0];
+            _imagesUp.sprite = _imagesMenu[1];
+            _imagesDown.sprite = _imagesMenu[1];
+        }
+        if (StatPers.LVLBooK > 3 && StatPers.LVLBooK <= 6)
+        {
+            _imagesBook.sprite = _imagesMenu[2];
+            _imagesUp.sprite = _imagesMenu[3];
+            _imagesDown.sprite = _imagesMenu[3];
+        }
+        if (StatPers.LVLBooK > 6 )
+        {
+            _imagesBook.sprite = _imagesMenu[4];
+            _imagesUp.sprite = _imagesMenu[5];
+            _imagesDown.sprite = _imagesMenu[5];
+        }
+    }
+
     private void Awake()
     {
         StatPers.LoadData();
@@ -37,7 +65,6 @@ public class PageManeger : MonoBehaviour
             _tutorial.first_tutorial = true;
             _tutorial.SaveData();
         }
-
         Buttoms[0].transform.DOScale(1.3f, 0.4f);
         Buttoms[1].transform.DOScale(1.0f, 0.4f);
         Buttoms[2].transform.DOScale(1.0f, 0.4f);
