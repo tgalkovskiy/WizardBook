8e448da2d05e886cfa52ce97ed1dc8062c4af7c3
tgalkovskiy <80895083+GalkovskiyTimur@users.noreply.github.com>
2021-05-07 01:53:14 +0300

    fixlang

--- Assets/NewBuild/scriptbleObj/ChoiesLanguege.cs
+++ Assets/NewBuild/scriptbleObj/ChoiesLanguege.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 
@@ -7,6 +8,52 @@ using UnityEngine;
 
 public class ChoiesLanguege : ScriptableObject
 {
+    public class DataLang
+    {
+        public int Languge1;
+        public int Languge2;
+    }
+    
     public int Languge1;
     public int Languge2;
+
+
+    public void SaveData()
+    {
+        string Path = System.IO.Path.Combine(Application.persistentDataPath, "SaveLang.Json");
+        DataLang data = new DataLang();
+        data.Languge1 = Languge1;
+        data.Languge2 = Languge2;
+        try
+        {
+            File.WriteAllText(Path, JsonUtility.ToJson(data));
+        }
+        catch
+        {
+            //Debug.Log("not Save");
+        }
+        finally
+        {
+            //Debug.Log("Save Done");
+        }
+    }
+
+    public void LoadData()
+    {
+        //Debug.Log(1);
+        string Path = System.IO.Path.Combine(Application.persistentDataPath, "SaveLang.Json");
+        if (File.Exists(Path))
+        {
+            DataLang data = new DataLang();
+            data = JsonUtility.FromJson<DataLang>(File.ReadAllText(Path));
+            Languge1 = data.Languge1;
+            Languge2 = data.Languge2;
+            //Debug.Log(2);
+        }
+        else
+        {
+            Languge1 = 1;
+            Languge2 = 0;
+        }
+    }
 }
